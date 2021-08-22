cc0e8980c9aa397fd65586a5269e0df2405f7edd
AristSolo <solomonwachira212@gmail.com>
2021-08-22 14:03:04 +0300

    Created a wallet table that can house user transactions for a sepcific business

---
+++ migrations/20210822103652-create-wallet.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Wallets', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      income: {
+        type: Sequelize.INTEGER
+      },
+      expense: {
+        type: Sequelize.INTEGER
+      },
+      business_id: {
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Wallets');
+  }
+};
